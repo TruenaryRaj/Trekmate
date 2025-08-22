acea9aca57cf9fa4766f55fbdbd6439801d097e7
TruenaryRaj <raj.truenary@gmail.com>
2025-08-22 10:38:06 +0545

    feat: completed all the create part

--- src/controllers/transportation-booking.ts
+++ src/controllers/transportation-booking.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { transportationBookingRepositories } from '../repositories';
+import { th } from 'zod/locales';
 
 export const transportationBookingController = {
     async createBooking(req: Request, res: Response) {
@@ -19,5 +20,61 @@ export const transportationBookingController = {
         } catch (error) {
             res.status(500).json({ error: 'Failed to create booking' });
         }
+    },
+
+    async getBookingsByTransportationId(req: Request, res: Response) {
+        const transportationId = parseInt(req.params.id);
+        const { page, limit, sortBy } = req.body;
+
+        if (!transportationId) {
+            throw new Error('Transportation ID is required');
+        }
+
+        try {
+            const bookings = await transportationBookingRepositories.getBookingsByTransportationId(transportationId, {
+                page,
+                limit,
+                sortBy
+            });
+            res.status(200).json(bookings);
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to retrieve bookings' });
+        }
+    },
+
+    async getBookingsByUserId(req: Request, res: Response) {
+        const userId = req.user?.id;
+        const { page, limit, sortBy } = req.body;
+
+        if (!userId) {
+            throw new Error('Transportation ID is required');
+        }
+
+        try {
+            const bookings = await transportationBookingRepositories.getBookingsByUserId(userId, {
+                page,
+                limit,
+                sortBy
+            });
+            res.status(200).json(bookings);
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to retrieve bookings' });
+        }
+    },
+
+    async getAllBookings(req: Request, res: Response) {
+        const { page, limit, sortBy } = req.body;
+
+        try {
+            const bookings = await transportationBookingRepositories.getAllBookings({
+                page,
+                limit,
+                sortBy
+            });
+            res.status(200).json(bookings);
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to retrieve all bookings' });
+        }
     }
+
 }
--- src/repositories/transportation-booking.repositories.ts
+++ src/repositories/transportation-booking.repositories.ts
@@ -1,7 +1,8 @@
-import { eq } from 'drizzle-orm';
+import { desc, eq } from 'drizzle-orm';
 import { db } from '../db/db';
 import { transportationBooking } from '../db/schema';
 import { TransportationBooking } from '../types/booking.types';
+import { PaginationInput } from '../types';
 export const transportationBookingRepositories = {
     async createBooking(input: TransportationBooking) {
        const { userId, transportationId, date} = input;
@@ -24,5 +25,46 @@ export const transportationBookingRepositories = {
         date
        }).where(eq(transportationBooking.userId, userId));
        return result.insertId;
-    }
+    },
+
+    async getBookingsByTransportationId(transportationId: number, input: PaginationInput) {
+        const { page =1 , limit = 5, sortBy } = input;
+        const offset = (page - 1) * limit;
+        try {
+        return await db.select().from(transportationBooking)
+        .where(eq(transportationBooking.transportationId, transportationId))
+        .limit(limit)
+        .offset(offset)
+        .orderBy(transportationBooking.date, sortBy === 'asc' ? (transportationBooking.createdAt) : (desc(transportationBooking.createdAt)));
+        } catch (error) {
+            throw new Error('Failed to retrieve bookings by transportation ID');
+        }
+    },
+
+    async getBookingsByUserId(userId: number, input: PaginationInput) {
+        const { page = 1, limit = 5, sortBy } = input;
+        const offset = (page - 1) * limit;
+        try {
+        return await db.select().from(transportationBooking)
+        .where(eq(transportationBooking.userId, userId))
+        .limit(limit)
+        .offset(offset)
+        .orderBy(transportationBooking.date, sortBy === 'asc' ? (transportationBooking.createdAt) : (desc(transportationBooking.createdAt)));
+        } catch (error) {
+            throw new Error('Failed to retrieve bookings by user ID');
+        }
+    },
+
+    async getAllBookings(input: PaginationInput) {
+        const { page = 1, limit = 5, sortBy } = input;
+        const offset = (page - 1) * limit;
+        try {
+        return await db.select().from(transportationBooking)
+        .limit(limit)
+        .offset(offset)
+        .orderBy(transportationBooking.date, sortBy === 'asc' ? (transportationBooking.createdAt) : (desc(transportationBooking.createdAt)));
+        } catch (error) {
+            throw new Error('Failed to retrieve all bookings');
+        }
+    },
 }
