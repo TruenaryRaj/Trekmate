cdfa6fb31f3da0a7bad766862a723ded838e8c54
TruenaryRaj <raj.truenary@gmail.com>
2025-11-12 21:57:04 +0545

    fix: remode authorization in verify email

--- src/routes/creation.routes.ts
+++ src/routes/creation.routes.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { userController } from "../controllers/user.controllers";
 import upload from "../utils/uploadHelper";
-import { authorize } from "../middleware/authorize.middleware";
 import { authenticateToken } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post('/signup', upload.array("images", 1), userController.signup);
-router.post('/verifyEmail', authenticateToken, authorize(['admin', 'user']), userController.verifyEmail);
+router.post('/verifyEmail', authenticateToken, userController.verifyEmail);
 router.post('/login', userController.login);
 
 export default router;
